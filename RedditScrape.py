e0d68ac05c9fc19f1bdd238eb36c272de9610ba5
Uglamator <78444658+Uglamator@users.noreply.github.com>
2021-02-03 22:34:59 +0000

    Update RedditScrape.py

--- RedditScrape.py
+++ RedditScrape.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Feb  1 17:17:59 2021
 
-@author: jorda
+@author: 
 """
 
 import praw
@@ -14,7 +14,7 @@ c = conn.cursor()
 #c.execute('CREATE TABLE comments (id,body,created_datetime,score)')
 conn.commit()
 pd.set_option("display.max_colwidth", 100000)
-reddit = praw.Reddit(client_id='JKuwICzPpSy0Ow' , client_secret= 'T3p1P7pdZ8x5opxv9RNOVHKdeOxsIw',user_agent='JFTESTEROO')
+reddit = praw.Reddit(client_id='JKuwICzPpSy0Ow' , client_secret= '',user_agent='JFTESTEROO')
 stream = reddit.subreddit('Cryptocurrency').stream.comments(skip_existing=True)
 
 for x in stream:
